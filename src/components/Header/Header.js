c3cae4f820a604025dc0941e7826af46fe0e4ebb
Maksym <maksym.sivko@gmail.com>
2021-06-01 21:54:05 +0300

    +

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { Icon } from 'elements/Icon/Icon';
+import { HeaderNavItem } from 'elements/HeaderNavItem/HeaderNavItem';
 
 import {
   HeaderContainer,
@@ -9,8 +10,13 @@ import {
   ControlBtn,
   LogoConntrol,
   HeaderNavList,
+  ConntrolLenguage,
+  LenguageBtn,
+  LenguageActive,
+  LenguageList,
+  LenguageListItem,
+  ControlBtnQuantity,
 } from './style';
-import { HeaderNavItem } from 'elements/HeaderNavItem/HeaderNavItem';
 
 const category = [
   {
@@ -37,78 +43,42 @@ export default function Header() {
       <HeaderContainer>
         <HeaderConntrol>
           <ControlBtn>
-            {/* onClick={openSidebarMenu}*/}
             <Icon name="menu" />
           </ControlBtn>
           <LogoConntrol>
-            {
-              <Link href={'/'}>
-                <a>
-                  <img src="image/logo.svg" alt="Logo" />
-                </a>
-              </Link>
-            }
+            <Link href={'/'}>
+              <a>
+                <img src="image/logo.svg" alt="Logo" />
+              </a>
+            </Link>
           </LogoConntrol>
         </HeaderConntrol>
         <HeaderNavList>
           <HeaderNavItem list={category} />
-
-          {/* {menu &&
-              menu.length != 0 &&
-              menu
-                .slice(0)
-                .reverse()
-                .map((e: any, index: number) => {
-                  return (
-                    <HeaderNavItem
-                      data={e}
-                      list={e.sub_categories}
-                      key={index}
-                    />
-                  );
-                })} */}
         </HeaderNavList>
-        <div className="header__control">
-          <div className="control__language">
-            <div className="language__btn">
-              <div className="language__active">language__active</div>
-              {/* <div className="language__active">{router.locale}</div> */}
-              {/* <Icon name="down" /> */}
-            </div>
-            <div className="language__list">
-              {/* {router.locales?.map((loc: any, i: any) => (
-                  <Link
-                    key={`language__list-item-key-${i}`}
-                    href={currentUrl}
-                    locale={loc}
-                  >
-                    <a
-                      className={`language__list-item ${
-                        loc === router.locale ? '-active' : ''
-                      } `}
-                    >
-                      {loc}
-                    </a>
-                  </Link>
-                ))} */}
-              language__list
-            </div>
-          </div>
-          <div
-            className="control__btn btn-cart"
-            // onClick={() => (quantity != null ? router.push('/cart') : {})}
-          >
-            {/* {quantity !== null ? ( */}
-            <>
-              {/* <div className="btn-cart__quantity">{quantity}</div> */}
-              <div className="btn-cart__quantity">quantity</div>
-              {/* <Icon name="cart-black" /> */}
-            </>
-            {/* ) : (
-                <Icon name="cart" />
-              )} */}
-          </div>
-        </div>
+        <HeaderConntrol>
+          <ConntrolLenguage>
+            <LenguageBtn>
+              <LenguageActive>ru</LenguageActive>
+              <Icon name="down" />
+            </LenguageBtn>
+            <LenguageList className="lenguage-list">
+              <Link href="/#">
+                <LenguageListItem className=" -active">ru</LenguageListItem>
+              </Link>
+              <Link href="/#">
+                <LenguageListItem>ua</LenguageListItem>
+              </Link>
+              <Link href="/#">
+                <LenguageListItem>us</LenguageListItem>
+              </Link>
+            </LenguageList>
+          </ConntrolLenguage>
+          <ControlBtn className="btn-cart">
+            {/* <ControlBtnQuantity>2</ControlBtnQuantity> */}
+            <Icon name="cart" />
+          </ControlBtn>
+        </HeaderConntrol>
       </HeaderContainer>
       <HeaderBasement />
     </>
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -14,6 +14,10 @@ export const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  @media (max-width: 374px) {
+    padding: 0 20px;
+  }
 `;
 
 export const HeaderBasement = styled.div`
@@ -34,10 +38,41 @@ export const ControlBtn = styled.div`
   align-items: center;
   margin-right: 12px;
 
-  .icon-menu {
+  .icon-menu,
+  .icon-cart {
     font-size: 23px;
     color: ${({ theme }) => theme.color.colorDark};
   }
+
+  &.btn-cart {
+    margin-left: 24px;
+    position: relative;
+
+    @media (max-width: 374px) {
+      margin-left: 15px;
+    }
+
+    &.-active {
+      .icon-cart {
+        color: #000;
+      }
+    }
+  }
+`;
+
+export const ControlBtnQuantity = styled.div`
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: BasisGrotesquePro-Bold;
+  color: ${({ theme }) => theme.color.colorWhite};
+  background: ${({ theme }) => theme.color.colorDark};
+  font-size: 13px;
+  top: 12px;
+  left: 11px;
 `;
 
 export const LogoConntrol = styled.div`
@@ -54,4 +89,82 @@ export const HeaderNavList = styled.div`
   max-width: 60%;
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: 1024px) {
+    display: none;
+    visibility: hidden;
+  }
+`;
+
+export const ConntrolLenguage = styled.div`
+  position: relative;
+
+  &:hover {
+    ::before {
+      position: absolute;
+      content: '';
+      width: 90px;
+      height: 60px;
+      left: -25px;
+      top: -10px;
+      cursor: pointer;
+    }
+
+    .lenguage-list {
+      border-top: 1px solid ${({ theme }) => theme.color.colorDark};
+      height: max-content;
+      padding: 12px 26px;
+    }
+  }
+`;
+
+export const LenguageBtn = styled.div`
+  position: relative;
+  cursor: pointer;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+`;
+
+export const LenguageActive = styled.div`
+  font-family: BasisGrotesquePro-Bold;
+  color: ${({ theme }) => theme.color.colorDark};
+  font-size: 13px;
+  text-transform: uppercase;
+  display: flex;
+  align-items: center;
+`;
+
+export const LenguageList = styled.div`
+  transition: 0.5s;
+  position: absolute;
+  cursor: pointer;
+  background: ${({ theme }) => theme.color.colorMainBackground};
+  flex-direction: column;
+  padding: 0px 26px;
+  /* border-top: 1px solid ${({ theme }) => theme.color.colorDark}; */
+  z-index: -1;
+  left: -25px;
+  top: 48px;
+  display: flex;
+  overflow: hidden;
+  height: 0;
+`;
+
+export const LenguageListItem = styled.a`
+  cursor: pointer;
+  font-family: BasisGrotesquePro-Bold;
+  color: ${({ theme }) => theme.color.colorGrayLight};
+  font-size: 13px;
+  text-transform: uppercase;
+  margin-bottom: 16px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  &.-active,
+  &:hover {
+    color: ${({ theme }) => theme.color.colorDark};
+  }
 `;
